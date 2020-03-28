574b2b8abea00ebb8e4f19ecde059593752cf739
pratik3098 <pratik3098@gmail.com>
2020-03-28 15:45:57 -0400

    contract successfully recieved ethers

--- test/1_test_multiSigWallet.js
+++ test/1_test_multiSigWallet.js
@@ -1,8 +1,18 @@
 const multiSigWallet = artifacts.require('multiSigWallet');
 
 contract("multiSigWallet", accounts => {
+   let wallet
+   const bal = web3.utils.toWei('20', 'ether');
+
+   before(() => {
+      return multiSigWallet.deployed().then(instance => {
+            wallet = instance;
+      });
+   });
    
     it("should be able to send balance to contract", async () => {
-          assert(1,1,"proper syntax")    
+          await wallet.recieve({value: bal})
+          let walletBalance= await wallet.getBalance();
+         assert.equal("20000000000000000000", walletBalance,"Ethers successfully recieved")    
      });
 })
